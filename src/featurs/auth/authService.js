888f73b27d89e9e60e49a42336c96ed78bcf23e9
burhandhar6004 <burhaneducation6004@gmail.com>
2024-05-14 09:56:39 +0530

    first commit

---
+++ src/featurs/auth/authService.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+const API_URL = "/api/user/";
+
+const register = async(userData) => {
+    const response = await axios.post("https://authentication-2-qgze.onrender.com/api/user/register" , userData)
+    localStorage.setItem("user", JSON.stringify(response.data))
+    return response.data
+}
+
+
+const login = async(userData) => {
+    const response = await axios.post("https://authentication-2-qgze.onrender.com/api/user/login", userData)
+    return response.data
+}
+
+
+const authService = {
+    register,
+    login,
+}
+
+
+export default authService ;
